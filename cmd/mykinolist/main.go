100736039e87b18e06c2b5fe13ef682acdc75a7a
Kirill Ryabov <sailor.kirill@gmail.com>
2023-06-14 14:18:09 +0700

    Add config file

--- cmd/mykinolist/main.go
+++ cmd/mykinolist/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/kiryu-dev/mykinolist/internal/app"
+	"github.com/spf13/viper"
 )
 
-const port = ":8080"
+const configPath = "./configs/config.yaml"
 
 func main() {
-	app.Run(port)
+	if err := loadConfigFile(); err != nil {
+		log.Fatal(err.Error())
+	}
+	app.Run(viper.GetString("port"))
+}
+
+func loadConfigFile() error {
+	viper.SetConfigFile(configPath)
+	return viper.ReadInConfig()
 }
