799980da1d42647f74cf28952d4e314c56dd4e69
Kirill Ryabov <sailor.kirill@gmail.com>
2023-06-19 20:35:47 +0700

    Add setting x-authorization header with access token and http-only cookie with refresh token

--- internal/controller/auth.go
+++ internal/controller/auth.go
@@ -11,6 +11,8 @@ import (
 	"github.com/kiryu-dev/mykinolist/internal/service"
 )
 
+const cookieMaxAge = 30 * 24 * 60 * 60 // 30 days
+
 type authHandler struct {
 	service service.AuthService
 }
@@ -22,7 +24,6 @@ func InitAuthRoutes(router *mux.Router, s service.AuthService) {
 }
 
 func (h *authHandler) signUp(w http.ResponseWriter, r *http.Request) {
-	startTime := time.Now()
 	req := new(model.SignUpUserDTO)
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 		resp := &errorResponse{err.Error()}
@@ -30,18 +31,25 @@ func (h *authHandler) signUp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer r.Body.Close()
+	startTime := time.Now()
 	user, tokens, err := h.service.SignUp(req)
+	log.Printf("elapsed time: %v", time.Since(startTime))
 	if err != nil {
 		resp := &errorResponse{err.Error()}
 		writeJSONResponse(w, http.StatusInternalServerError, resp)
 		return
 	}
+	w.Header().Add("X-Authorization", tokens.AccessToken)
+	http.SetCookie(w, &http.Cookie{
+		Name:     "refreshToken",
+		Value:    tokens.RefreshToken,
+		MaxAge:   cookieMaxAge,
+		HttpOnly: true,
+	})
 	writeJSONResponse(w, http.StatusOK, map[string]any{"user": user, "tokens": tokens})
-	log.Printf("elapsed time: %v", time.Since(startTime))
 }
 
 func (h *authHandler) signIn(w http.ResponseWriter, r *http.Request) {
-	startTime := time.Now()
 	req := new(model.SignInUserDTO)
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 		resp := &errorResponse{err.Error()}
@@ -49,12 +57,20 @@ func (h *authHandler) signIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer r.Body.Close()
+	startTime := time.Now()
 	user, tokens, err := h.service.SignIn(req)
+	log.Printf("elapsed time: %v", time.Since(startTime))
 	if err != nil {
 		resp := &errorResponse{err.Error()}
 		writeJSONResponse(w, http.StatusInternalServerError, resp)
 		return
 	}
+	w.Header().Add("X-Authorization", tokens.AccessToken)
+	http.SetCookie(w, &http.Cookie{
+		Name:     "refreshToken",
+		Value:    tokens.RefreshToken,
+		MaxAge:   cookieMaxAge,
+		HttpOnly: true,
+	})
 	writeJSONResponse(w, http.StatusOK, map[string]any{"user": user, "tokens": tokens})
-	log.Printf("elapsed time: %v", time.Since(startTime))
 }
