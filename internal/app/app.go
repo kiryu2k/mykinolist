96641cdfc5e204d5ebdee55999f45ca091e4db98
Kirill Ryabov <sailor.kirill@gmail.com>
2023-06-26 00:00:25 +0700

    Add graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,8 +1,12 @@
 package app
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/kiryu-dev/mykinolist/internal/config"
 	"github.com/kiryu-dev/mykinolist/internal/controller"
@@ -16,7 +20,11 @@ func Run(config *config.Config) {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	defer db.Close()
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
 	var (
 		repo     = repository.New(db)
 		services = service.New(
@@ -29,7 +37,21 @@ func Run(config *config.Config) {
 		)
 		controller = controller.New(services.AuthService, services.ListService)
 	)
-	if err := http.ListenAndServe(config.ListeningPort, controller); err != nil {
+	server := http.Server{
+		Addr:    config.ListeningPort,
+		Handler: controller,
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+	log.Println("mykinolist 😼")
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	log.Println("see you again, dude 😼")
+	if err := server.Shutdown(context.Background()); err != nil {
 		log.Fatal(err.Error())
 	}
 }
